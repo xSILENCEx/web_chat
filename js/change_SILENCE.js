6d175bebca49b99f9b2ae5bf5e674b8e01fa7295
xSILENCEx <894659061@qq.com>
2019-05-10 21:32:39 +0800

    修复连接提示\

--- js/change_SILENCE.js
+++ js/change_SILENCE.js
@@ -1,6 +1,6 @@
 function setConnectError(error) {
     var c = document.getElementById("connect");
-    c.innerHTML = "无法连接到服务器";
+    c.value = "无法连接到服务器";
     c.title = "无法连接到服务器";
     c.style.backgroundColor = "rgba(255, 120, 120, 1.00)";
 }
@@ -23,7 +23,7 @@ function errorInfo(error) {
 
 function connectSuccess() {
     var c = document.getElementById("connect");
-    c.innerHTML = "服务器连接成功";
+    c.value = "服务器连接成功";
     c.title = "已连接到服务器";
     c.style.backgroundColor = "rgba(115, 204, 102, 1.00)";
 }
