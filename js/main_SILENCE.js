86b34f58f56f5b15d0ca170838b75f32dd4fc87f
xSILENCEx <894659061@qq.com>
2019-05-12 16:11:54 +0800

    修改发送逻辑

--- js/main_SILENCE.js
+++ js/main_SILENCE.js
@@ -1,15 +1,15 @@
-//清空输入框
+///////////////////////////////////////////////////////////////清空输入框
 function clearEdit() {
     document.getElementById("edit").value = "";
     document.getElementById("edit").focus();
 }
 
-//获取输入框内容
+////////////////////////////////////////////////////////////获取输入框内容
 function getEdit() {
     return document.getElementById("edit").value;
 }
 
-//显示接收的消息
+/////////////////////////////////////////////////////////显示左边的消息
 function leftSend(head, name, msg) {
     var newMsg = document.createElement("div");
     newMsg.setAttribute("class", "msg-item");
@@ -35,45 +35,52 @@ function leftSend(head, name, msg) {
     scrollToBottom(newMsg);
 }
 
-//当前用户发送消息
+////////////////////////////////////////////////////////////显示右边的消息
 function rightSend(head, name, msg) {
-    //向服务器发送消息
-    SendMessageToServer(getEdit());
-    clearEdit();
+    var newMsg = document.createElement("div");
+    newMsg.setAttribute("class", "msg-item");
+
+    var h = document.createElement("img");
+    h.setAttribute("class", "head-img2");
+    h.setAttribute("src", head);
+
+    var lName = document.createElement("div");
+    lName.setAttribute("class", "user-name2");
+    lName.innerHTML = name;
+
+    var lMsg = document.createElement("div");
+    lMsg.setAttribute("class", "msg-box2 theme");
+    lMsg.innerHTML = msg;
+
+    newMsg.appendChild(h);
+    newMsg.appendChild(lName);
+    newMsg.appendChild(lMsg);
+
+    var chatBox = document.getElementById("chat-box");
+    chatBox.appendChild(newMsg);
+    scrollToBottom(newMsg);
 }
 
-//当前用户发送消息的动作
-function meSend() {
+////////////////////////////////////////////////////////////当前用户发送消息的动作
+function meSend(msg) {
+    //////////////////////////////////////////////////////////////清空键值
     event.keyCode = 0;
     event.returnValue = false;
-    rightSend('../img/def-boy.svg', '匿名游客');
+    if (msg != "") {
+        ////////////////////////////////////////////////////////向服务器发送消息
+        SendMessageToServer(getEdit());
+        clearEdit();
+    } else {
+        console.log("内容为空");
+    }
 }
 
-//接收来自服务器的消息，复制原rightSend(head, name)函数
+////////////////////////////////////接收来自服务器的消息，复制原rightSend(head, name)函数
 function ReceiveByServer(head, name, msg) {
-    if (msg.length != 0) {
-        var newMsg = document.createElement("div");
-        newMsg.setAttribute("class", "msg-item");
-
-        var h = document.createElement("img");
-        h.setAttribute("class", "head-img2");
-        h.setAttribute("src", head);
-
-        var lName = document.createElement("div");
-        lName.setAttribute("class", "user-name2");
-        lName.innerHTML = name;
-
-        var lMsg = document.createElement("div");
-        lMsg.setAttribute("class", "msg-box2 theme");
-        lMsg.innerHTML = msg;
-
-        newMsg.appendChild(h);
-        newMsg.appendChild(lName);
-        newMsg.appendChild(lMsg);
-
-        var chatBox = document.getElementById("chat-box");
-        chatBox.appendChild(newMsg);
-        scrollToBottom(newMsg);
+    if (msg != "") {
+        rightSend(head, name, msg);
+    } else {
+        leftSend(head, name, msg);
     }
 }
 
@@ -92,7 +99,7 @@ onload = function () {
 
     ///////////////////////////////////////////////////////////////////////////////////////点击发送按钮
     document.getElementById("send").onmousedown = function () {
-        meSend();
+        meSend(getEdit());
     }
 
     //////////////////////////////////////////////////////////////////////////////////自动发送系统提示信息
