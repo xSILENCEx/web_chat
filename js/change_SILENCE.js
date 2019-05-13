1c67b2c8b69588d30d94c0921acb19f2323a7d6d
xSILENCEx <894659061@qq.com>
2019-05-13 18:33:12 +0800

    PC端添加发送框拖动

--- js/change_SILENCE.js
+++ js/change_SILENCE.js
@@ -1,5 +1,5 @@
 onresize = function () {
-    //屏幕大小是否合适
+    ////////////////////////////////////////////////////////////////////////////////////屏幕大小是否合适
     if (isSmall()) {
         smallScreen();
     } else {
@@ -8,7 +8,7 @@ onresize = function () {
     }
 }
 
-//发送消息后滚动到底部
+/////////////////////////////////////////////////////////////////////////////////////发送消息后滚动到底部
 function scrollToBottom(obj) {
     obj.scrollIntoView({
         block: "start",
@@ -16,21 +16,21 @@ function scrollToBottom(obj) {
     });
 }
 
-//判断屏幕宽度是否大于2000
+///////////////////////////////////////////////////////////////////////////////////判断屏幕宽度是否大于2000
 function isSmall() {
     var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
     if (w < 2000) return true;
     else return false;
 }
 
-//判断屏幕高度是否大于1150
+//////////////////////////////////////////////////////////////////////////////////判断屏幕高度是否大于1150
 function isHigher() {
     var h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
     if (h > 1150) return true;
     else return false;
 }
 
-//屏幕足够宽时调用此方法
+///////////////////////////////////////////////////////////////////////////////////////屏幕足够宽时调用此方法
 function bigScreen(width) {
     var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
     document.getElementById("reg-login").innerHTML = "<div style='font-size:16px'>一个简单的群聊网站<div>";
@@ -44,7 +44,7 @@ function bigScreen(width) {
     document.getElementById("whole").style.transform = "translateX(0px)";
 }
 
-//小屏幕调用此方法
+//////////////////////////////////////////////////////////////////////////////////////////////////小屏幕调用此方法
 function smallScreen() {
     document.getElementById("reg-login").innerHTML = "设置";
     closeRight();
@@ -53,7 +53,7 @@ function smallScreen() {
     isLeftOpen = false;
 }
 
-//服务器连接失败的提示信息
+///////////////////////////////////////////////////////////////////////////////////////////////服务器连接失败的提示信息
 function setConnectError(error) {
     var c = document.getElementById("connect");
     c.value = "无法连接到服务器";
@@ -61,7 +61,7 @@ function setConnectError(error) {
     c.style.backgroundColor = "rgba(255, 120, 120, 1.00)";
 }
 
-//服务器断开后点击发送给予反馈
+/////////////////////////////////////////////////////////////////////////////////////////////服务器断开后点击发送给予反馈
 var isToasted = false;
 
 function errorInfo(error) {
@@ -78,10 +78,52 @@ function errorInfo(error) {
     }, 100);
 }
 
-//服务器连接成功的提示信息
+///////////////////////////////////////////////////////////////////////////////////////////////服务器连接成功的提示信息
 function connectSuccess() {
     var c = document.getElementById("connect");
     c.value = "服务器连接成功";
     c.title = "已连接到服务器";
     c.style.backgroundColor = "rgba(118, 178, 74, 1.00)";
 }
+
+var moveBar = document.getElementById("tool-bar");
+var chatBox = document.getElementById("chat-box");
+var isPress = false;
+var barStartY = null;
+var oldY = null;
+
+moveBar.addEventListener("mousedown", function (e) {
+    if (!isPress) {
+        barStartY = e.clientY;
+        oldY = parseInt(getComputedStyle(chatBox, null).getPropertyValue("bottom"));
+        console.log("在工具栏按下鼠标\n纵坐标:" + barStartY);
+        isPress = true;
+    }
+});
+
+document.addEventListener("mousemove", function (e) {
+
+    if (isPress) {
+        var max = parseInt(getComputedStyle(chatBox, null).getPropertyValue("bottom"));
+        var editBox = document.getElementById("edit-box");
+
+        chatBox.style.bottom = oldY - (e.clientY - barStartY) + "px";
+        editBox.style.height = oldY - (e.clientY - barStartY) + "px";
+        if (max > 610) {
+            chatBox.style.bottom = "600px";
+            editBox.style.height = "600px";
+            isPress = false;
+        } else if (max < 90) {
+            chatBox.style.bottom = "100px";
+            editBox.style.height = "100px";
+            isPress = false;
+        }
+    }
+});
+
+document.addEventListener("mouseup", function (e) {
+    if (isPress) {
+        console.log("抬起鼠标\n变化纵坐标:" + (e.clientY - barStartY));
+        isPress = false;
+    }
+});
