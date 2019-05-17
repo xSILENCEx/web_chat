27622c612faa818defeef0ffcc134e262ff04021
TwT <3300215540@qq.com>
2019-05-17 15:19:25 +0800

    更新说明: 同步最新WEB UI HTTP服务器:添加配置文件,增加用户文件请求路由. 用户管理:添加数据库 WebScoket服务器:优化内部消息转发,完成消息/图片/文件发送,用户登录/注册相关功能.

---
+++ js/system_SILENCE.js
@@ -0,0 +1,112 @@
+//////加载完成后的动作
+onload = function () {
+
+    ////////连接到服务器
+    ConnectToServer();
+
+    //////屏幕大小是否合适并作相应反馈
+    softWindow();
+
+    /////自动发送系统提示信息
+    leftSend("../img/system.svg", "系统提示", "欢迎使用简聊Web！试试左滑右滑~<br>Ctrl+Enter发送消息，点击logo打开左边栏(大屏幕忽略此条)。");
+
+}
+
+/////////接收来自服务器的消息
+function ReceiveByServer(self, head, name, msg) {
+    if (self) {
+        rightSend(head, name, msg);
+    } else {
+        leftSend(head, name, msg);
+    }
+}
+
+/////弹出提示信息，支持富文本
+function openTips(type, content) {
+    var tips = document.getElementById("tips");
+    tips.style.transform = "scale(1.0)";
+    tips.style.opacity = "1.0";
+    setTimeout(function () {
+        tips.style.backgroundColor = "rgba(0,0,0,0.5)";
+    }, 500);
+    putInfo(type, content);
+}
+
+/////刷新用户列表
+function refreshUserList() {
+    console.log("刷新用户列表");
+}
+
+/////////登录成功后调用此方法
+function logInfo(info) { ///////传入一个json字符串数组，包含所有用户的所有信息
+    console.log("返回信息:" + info);
+    closeRegLogBox();
+    isLogBoxOpen = false;
+
+}
+
+////文件发送
+document.getElementById("file-box").onclick = function () {
+    document.getElementById("files2").click();
+}
+document.getElementById("files2").onchange = function () {
+
+    var file = document.getElementById("files2").files[0];
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function (evt) {
+        setTimeout(function () {
+            var jsonObject = {}
+            jsonObject.filename = file.name;
+            jsonObject.file = evt.target.result;
+            SendMessageToServer(3, JSON.stringify(jsonObject))
+        }, 1000);
+    }
+    document.getElementById("files2").value = "";
+}
+
+/////图片选择与发送
+document.getElementById("pic-box").onclick = function () {
+    document.getElementById("files1").click();
+}
+document.getElementById("files1").onchange = function () {
+    var file = document.getElementById("files1").files[0];
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function (evt) {
+        setTimeout(function () {
+            var jsonObject = {}
+            jsonObject.filename = file.name;
+            jsonObject.file = evt.target.result;
+            SendMessageToServer(2, JSON.stringify(jsonObject))
+        }, 1000);
+
+    }
+    document.getElementById("files1").value = "";
+}
+
+/////选择头像时调用此方法
+function choosePic() {
+    console.log("选择头像");
+}
+
+//点击登录注册按钮
+document.getElementById("logBtn").addEventListener("click", function (e) {
+    if (this.value == "确认登录") {
+        console.log("用户名 : " + getUserName() + "\n" + "密码 : " + getPsw());
+        UserLogin(getUserName(), getPsw());
+    } else {
+        console.log("用户名 : " + getUserName() + "\n" + "密码 : " + getPsw());
+        if (checkPsw()) {
+            UserRegister(getUserName(), getPsw());
+        } else {
+            console.log("密码不一致");
+        }
+    }
+    e.stopPropagation();
+});
+
+/////获取服务器端口信息
+document.getElementById("net-submit").addEventListener("click", function () {
+    console.log(getServerInfo());
+});
