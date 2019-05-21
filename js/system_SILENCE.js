64f9aa32b4db7f5fef1b4ae6fa7014e7ac3fdd9e
TwT <3300215540@qq.com>
2019-05-21 08:31:35 +0800

    Squashed 'web_chat/' changes from fe5da8e..2494b9b
    2494b9b 修改登录逻辑
    6e9ce60 动态刷新在线用户列表
    9f417ea 更改部分变量名，添加注册成功的反馈
    f7bbd5e 解决图片显示问题
    87c5b68 Merge branch 'master' of https://github.com/xSILENCEx/web_chat
    7a89973 修改细节
    a626357 Merge pull request #5 from o-TwT-o/master
    5c31e94 更改个人地址.添加图片/文件发送功能,服务器反馈提示.
    28f26b0 分离接口
    ba8ef0d 修改细节
    c94ca59 整合js，修复弹窗Bug
    6d97ca6 修改细节
    658be5d 规范化id名
    7b8a0fb 暂时这样
    47c73cf 解决设置项与用户列表共存问题
    171a2ae 修改细节
    1681d4f 修改细节
    cc11c1c 添加弹窗
    fb5539c 路径修正
    50990ed 登录成功后调用logInfo(info);方法
    9e8a2d0 Merge pull request #3 from o-TwT-o/master
    7fc1797 Merge branch 'master' into master
    323f66e 添加手机端拖动输入框
    1c67b2c PC端添加发送框拖动
    efb2a0c 添加登录成功后的反馈logSuccess();
    49043c1 连接注册/登录接口
    a4e4e5b 完善用户数据库
    a4a5388 修改细节
    70607fc 添加登录接口
    808c7df 添加消息类型.优化数据转发.
    8fdf67d 确定大屏适配方案
    86b34f5 修改发送逻辑
    91bf6f1 修改细节
    6886ff5 重构js
    108fe85 修复大屏适配BUG，整理代码
    b665684 调整大屏适配方案
    8e7094a 改进聊天框
    6d175be 修复连接提示\
    d5d8b24 完成服务器ip和端口修改选项，准备设计用户列表
    337a5c7 大致完成登录框
    871a2d3 修改id
    a06405b 解决id冲突
    
    git-subtree-dir: web_chat
    git-subtree-split: 2494b9b1aa4325e567d71a7acb9ece4b13864e7b

---
+++ js/system_SILENCE.js
@@ -0,0 +1,135 @@
+//////加载完成后的动作
+onload = function () {
+
+    ////////连接到服务器
+    ConnectToServer();
+
+    //////屏幕大小是否合适并作相应反馈
+    softWindow();
+
+    /////自动发送系统提示信息
+    leftSend("../img/system.svg", "系统提示", "欢迎使用简聊Web！试试左滑右滑~<br>Ctrl+Enter发送消息，点击logo打开左边栏(大屏幕忽略此条)。");
+
+}
+
+/////////接收来自服务器的消息
+function ReceiveByServer(self, head, name, msg) {
+    if (self) {
+        rightSend(head, name, msg);
+    } else {
+        leftSend(head, name, msg);
+    }
+}
+
+/////弹出提示信息，支持富文本
+function openTips(type, content) {
+    var tips = document.getElementById("tips");
+    tips.style.transform = "scale(1.0)";
+    tips.style.opacity = "1.0";
+    setTimeout(function () {
+        tips.style.backgroundColor = "rgba(0,0,0,0.5)";
+    }, 500);
+    putInfo(type, content);
+}
+
+/////刷新用户列表
+function refreshUserList(info) {
+    var user = JSON.parse(info);
+    var userCount = user[0].loginUserSize;
+    var list = document.getElementById("userList");
+    list.innerHTML = "<p class=\"menu-title\" style=\"margin-bottom: 0px;font-size: 90%\">在线用户列表</p>";
+    for (var i = 1; i <= userCount; i++) {
+        var sign = user[i].UserProfile == "" ? "这个人什么都没留下" : user[i].UserProfile;
+        addUserItem(list, user[i].UserName, sign, "/img/def-boy.svg");
+    }
+}
+
+/////////登录成功后调用此方法
+function logInfo(info) { ///////传入一个json字符串数组，包含用户的所有信息
+    changeMyInfo(JSON.parse(info));
+    isLogin = true;
+    closeRegLogBox();
+    var h = document.getElementById("myHead");
+    h.removeEventListener("click", openRegLogBox);
+    h.addEventListener("click", function (e) {
+        openUser();
+    });
+    document.getElementById("username").value = "";
+    document.getElementById("password").value = "";
+    document.getElementById("checkPsw").value = "";
+
+    isLogBoxOpen = false;
+}
+
+/////////注册成功后调用此方法
+function regInfo(info) { ///////传入一个json字符串数组，包含用户的所有信息
+    console.log("返回信息:" + info);
+    document.getElementById("checkPsw").value = "";
+    changeToLog();
+}
+
+////文件发送
+document.getElementById("file-box").onclick = function () {
+    document.getElementById("files2").click();
+}
+document.getElementById("files2").onchange = function () {
+
+    var file = document.getElementById("files2").files[0];
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function (evt) {
+        setTimeout(function () {
+            var jsonObject = {}
+            jsonObject.filename = file.name;
+            jsonObject.file = evt.target.result;
+            SendMessageToServer(3, JSON.stringify(jsonObject))
+        }, 1000);
+    }
+    document.getElementById("files2").value = "";
+}
+
+/////图片选择与发送
+document.getElementById("pic-box").onclick = function () {
+    document.getElementById("files1").click();
+}
+document.getElementById("files1").onchange = function () {
+    var file = document.getElementById("files1").files[0];
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function (evt) {
+        setTimeout(function () {
+            var jsonObject = {}
+            jsonObject.filename = file.name;
+            jsonObject.file = evt.target.result;
+            SendMessageToServer(2, JSON.stringify(jsonObject))
+        }, 1000);
+
+    }
+    document.getElementById("files1").value = "";
+}
+
+/////选择头像时调用此方法
+function choosePic() {
+    console.log("选择头像");
+}
+
+//点击登录注册按钮
+document.getElementById("logBtn").addEventListener("click", function (e) {
+    if (this.value == "确认登录") {
+        console.log("用户名 : " + getUserName() + "\n" + "密码 : " + getPsw());
+        UserLogin(getUserName(), getPsw());
+    } else {
+        console.log("用户名 : " + getUserName() + "\n" + "密码 : " + getPsw());
+        if (checkPsw()) {
+            UserRegister(getUserName(), getPsw());
+        } else {
+            openTips(3, "密码不一致");
+        }
+    }
+    e.stopPropagation();
+});
+
+/////获取服务器端口信息
+document.getElementById("net-submit").addEventListener("click", function () {
+    console.log(getServerInfo());
+});
